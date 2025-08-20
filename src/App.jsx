2c4268ddcdae464363375360ef81fd44ce5fc558
ClaraGrigolon <claragrigolon@gmail.com>
2025-08-20 10:46:48 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,32 @@ function App() {
         
         {/* Card 1 */}
         <div className="card">
-          <img src={teniscat1} alt="" />
+          <img src={teniscat1} alt="" className="img-card"/>
           <h2>Tênis Speedcat Wine Club Feminino</h2>
           <p className="desc">O PUMA Speedcat tem sido um ícone da cultura automobilística e do estilo de rua por décadas. O mundo o conheceu inicialmente como um calçado de corrida ultra-fino, projetado para reduzir milissegundos nos tempos de volta.</p>
           <p className="classificacao"> ★ ★ ★ ★ ☆ (482)</p>
           <p className="preco">R$ 699,99</p>
-          <p className="off">5%</p>
+          <p className="off">- 5%</p>
+        </div>
+
+        {/* Card 2 */}
+        <div className="card">
+          <img src={teniscat1} alt="" className="img-card"/>
+          <h2>Tênis Speedcat Wine Club Feminino</h2>
+          <p className="desc">O PUMA Speedcat tem sido um ícone da cultura automobilística e do estilo de rua por décadas. O mundo o conheceu inicialmente como um calçado de corrida ultra-fino, projetado para reduzir milissegundos nos tempos de volta.</p>
+          <p className="classificacao"> ★ ★ ★ ★ ☆ (482)</p>
+          <p className="preco">R$ 699,99</p>
+          <p className="off">- 5%</p>
+        </div>
+
+        {/* Card 3 */}
+        <div className="card">
+          <img src={teniscat1} alt="" className="img-card"/>
+          <h2>Tênis Speedcat Wine Club Feminino</h2>
+          <p className="desc">O PUMA Speedcat tem sido um ícone da cultura automobilística e do estilo de rua por décadas. O mundo o conheceu inicialmente como um calçado de corrida ultra-fino, projetado para reduzir milissegundos nos tempos de volta.</p>
+          <p className="classificacao"> ★ ★ ★ ★ ☆ (482)</p>
+          <p className="preco">R$ 699,99</p>
+          <p className="off">- 5%</p>
         </div>
 
       </section>
